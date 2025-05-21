f4b9bb1ffe30c0a078227d5ff535a22be2f4f249
MichaelLong <michaellong971105@gmail.com>
2025-05-21 21:07:06 +1200

    Complete the random-forest model and integrate it into streamlit 完成random-forest模型，并整合到streamlit中

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # app.py
-
 import streamlit as st
-import pandas as pd
 from prediction_model import predict_player_value
 
 st.set_page_config(page_title="Player Value Estimator", layout="centered")
