4049cebc5efc369a9953715207c62ba9395e9fee
MichaelLong <michaellong971105@gmail.com>
2025-05-28 19:03:34 +1200

    Modify the code structure 修改代码结构

--- components/download_match_data.py
+++ components/download_match_data.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-start_season = 3
+start_season = 7
 end_season = 25
 
 ENGLAND = 'E0'
@@ -13,9 +13,9 @@ FRANCE = 'F1'
 leagues = [ENGLAND, GERMANY, ITALY, SPAIN, FRANCE]
 
 
-def download_season_data(league, season_code, year, save_dir):
+def download_season_data(league, season_code, year, save_dir, name):
     url = f"https://www.football-data.co.uk/mmz4281/{season_code}/{league}.csv"
-    file_path = os.path.join(save_dir, f"20{year:02d}-20{year + 1:02d}.csv")
+    file_path = os.path.join(save_dir, f"{name}_20{year:02d}-20{year + 1:02d}.csv")
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -29,17 +29,21 @@ def download_season_data(league, season_code, year, save_dir):
 # 遍历所有赛季
 for league in leagues:
     if league == ENGLAND:
-        save_dir = "data/match data/England"
+        save_dir = "../data/match_data/England"
+        name = 'England'
     elif league == GERMANY:
-        save_dir = "data/match data/Germany"
+        save_dir = "../data/match_data/Germany"
+        name = 'Germany'
     elif league == ITALY:
-        save_dir = "data/match data/Italy"
-
+        save_dir = "../data/match_data/Italy"
+        name = 'Italy'
     elif league == SPAIN:
-        save_dir = "data/match data/Spain"
+        save_dir = "../data/match_data/Spain"
+        name = 'Spain'
     else:
-        save_dir = "data/match data/France"
+        save_dir = "../data/match_data/France"
+        name = 'France'
     os.makedirs(save_dir, exist_ok=True)
     for year in range(start_season, end_season):
         season_code = f"{year:02d}{year + 1:02d}"  # e.g., '0304', '0405', ..., '2324'
-        download_season_data(league, season_code, year, save_dir)
+        download_season_data(league, season_code, year, save_dir, name)
